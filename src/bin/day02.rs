f3ff549d276d3270ba2b09b30cc5857643a4ba4d
James Youngman <james@youngman.org>
2020-12-28 23:35:49 +0000

    Make the 2020 Day 02 code slightly more idiomatic.

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -5,14 +5,10 @@ use std::io::BufRead;
 use regex::Regex;
 
 
-fn atpos(n: usize, ch: &str, passwd: &str) -> usize {
-    match passwd.get(n-1..n) {
-	Some(c1) => {
-	    if ch == c1 {
-		1
-	    } else {
-		0
-	    }
+fn atpos(pos: usize, expected: &str, passwd: &str) -> usize {
+    match passwd.get(pos-1..pos) {
+	Some(got) => {
+	    (expected == got) as usize
 	}
 	None => 0,
     }
@@ -20,19 +16,15 @@ fn atpos(n: usize, ch: &str, passwd: &str) -> usize {
 
 
 fn valid2(n1: usize, n2: usize, ch: &str, passwd: &str) -> bool {
-    return atpos(n1, ch, passwd) + atpos(n2, ch, passwd) == 1;
+    return (atpos(n1, ch, passwd) + atpos(n2, ch, passwd)) == 1;
 }
 
 fn valid1(n1: usize, n2: usize, ch: &str, passwd: &str) -> bool {
     let actual = passwd.matches(ch).count();
-    if actual >= n1 && actual <= n2 {
-	return true;
-    } else {
-	return false;
-    }
+    return actual >= n1 && actual <= n2;
 }
 
-fn main() {
+fn run() -> Result<(), std::io::Error> {
     // Example input lines:
     // 1-3 a: abcde
     // 1-3 b: cdefg
@@ -54,27 +46,36 @@ fn main() {
 		    Some(cap) => {
 			let n1: usize = cap[1].parse().unwrap();
 			let n2: usize = cap[2].parse().unwrap();
+			if n1 < 1 || n2 < 1 {
+			    panic!("{} is not a valid rule", s);
+			}
 			let ch = &cap[3];
 			let password = &cap[4];
 			if valid1(n1, n2, ch, password) {
 			    count1 += 1;
 			}
-			if n1 < 1 || n2 < 1 {
-			    panic!("{} is not a valid part 2 rule", s);
-			} else {
-			    if valid2(n1, n2, ch, password) {
-				count2 += 1;
-			    }
+			if valid2(n1, n2, ch, password) {
+			    count2 += 1;
 			}
 		    }
 		}
 	    }
 	    Err(e) => {
-		println!("error: {}", e);
-		return;
+		return Err(e);
 	    }
 	}
     }
     println!("Part 1: {} valid passwords out of {}", count1, total);
     println!("Part 2: {} valid passwords out of {}", count2, total);
+    return Ok(());
+}
+
+fn main() {
+    std::process::exit(match run() {
+	Ok(_) => 0,
+	Err(err) => {
+	    eprintln!("error: {:?}", err);
+	    1
+	}
+    });
 }
