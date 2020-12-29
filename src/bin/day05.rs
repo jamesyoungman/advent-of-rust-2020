d244ad859ba5038a58dbba178220e0f2aca998fe
James Youngman <james@youngman.org>
2020-12-29 13:58:56 +0000

    Day 05: condense the code slightly.

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -4,10 +4,10 @@ use std::collections::BTreeSet;
 
 fn partition(mut lower_incl: i32, mut upper_excl: i32,
 	     lower_directive: char, upper_directive: char,
-	     choices: &str) -> (i32, i32) {
+	     choices: &str) -> i32 {
     for choice in choices.chars() {
 	if upper_excl == lower_incl + 1 {
-	    panic!("we already have the answet but there are more directives remaining");
+	    panic!("overdetermined: we already have the answer but there are more directives remaining");
 	}
 	let mid = (upper_excl + lower_incl) / 2;
 	if choice == lower_directive {
@@ -19,27 +19,19 @@ fn partition(mut lower_incl: i32, mut upper_excl: i32,
 		   choice, choices);
 	}
     }
-    return (lower_incl, upper_excl)
-}
-
-fn single_value(partition_result: (i32, i32)) -> i32 {
-    match partition_result {
-	(lower_incl, upper_excl) => {
-	    if upper_excl == lower_incl + 1 {
-		return lower_incl
-	    }
-	    panic!("single_value: gap is not 1");
-	}
+    if upper_excl == lower_incl + 1 {
+	return lower_incl
     }
-}
-
-fn decode_seat(directions: &str) -> (i32, i32) {
-    return (single_value(partition(0, 128, 'F', 'B', &directions[0..7])),
-	    single_value(partition(0, 8, 'L', 'R', &directions[7..])))
+    panic!("partition: underdetermined, unsuffiicient directives");
 }
 
 fn seat_id(row: i32, col: i32) -> i32 {
     row * 8 + col
+}
+
+fn decode_seat(directions: &str) -> i32 {
+    return seat_id(partition(0, 128, 'F', 'B', &directions[0..7]),
+		   partition(0, 8, 'L', 'R', &directions[7..]))
 }
 
 fn part1(seats: &BTreeSet<i32>) {
@@ -65,11 +57,8 @@ fn part2(seats: &BTreeSet<i32>) {
 }
 
 fn run() -> Result<(), std::io::Error> {
-    let reader = io::BufReader::new(io::stdin());
-    let seats: BTreeSet<i32> = reader.lines()
-	.map(|l| l.unwrap())
-	.map(|l| decode_seat(&l.as_str()))
-	.map(|(row, col)| seat_id(row, col))
+    let seats: BTreeSet<i32> = io::BufReader::new(io::stdin()).lines()
+	.map(|l| decode_seat(&l.unwrap().as_str()))
 	.collect();
     part1(&seats);
     part2(&seats);
