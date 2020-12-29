248c304990084efeb510fadbaf8b83c8ade6d3a0
James Youngman <james@youngman.org>
2020-12-29 15:25:40 +0000

    Day 05: eliminate use of "panic!".

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -4,10 +4,10 @@ use std::collections::BTreeSet;
 
 fn partition(mut lower_incl: i32, mut upper_excl: i32,
 	     lower_directive: char, upper_directive: char,
-	     choices: &str) -> i32 {
+	     choices: &str) -> Result<i32, &'static str> {
     for choice in choices.chars() {
 	if upper_excl == lower_incl + 1 {
-	    panic!("overdetermined: we already have the answer but there are more directives remaining");
+	    return Err("overdetermined: too many directives");
 	}
 	let mid = (upper_excl + lower_incl) / 2;
 	if choice == lower_directive {
@@ -15,59 +15,61 @@ fn partition(mut lower_incl: i32, mut upper_excl: i32,
 	} else if choice == upper_directive {
 	    lower_incl = mid
 	} else {
-	    panic!("unexpected choice '{}' in '{}'",
-		   choice, choices);
+	    return Err("invalid choice");
 	}
     }
     if upper_excl == lower_incl + 1 {
-	return lower_incl
+	return Ok(lower_incl)
     }
-    panic!("partition: underdetermined, unsuffiicient directives");
+    return Err("undetermined, insuffiicient directives");
 }
 
 fn seat_id(row: i32, col: i32) -> i32 {
     row * 8 + col
 }
 
-fn decode_seat(directions: &str) -> i32 {
-    return seat_id(partition(0, 128, 'F', 'B', &directions[0..7]),
-		   partition(0, 8, 'L', 'R', &directions[7..]))
-}
-
-fn part1(seats: &BTreeSet<i32>) {
-    match seats.iter().next_back() {
-	Some(n) => {
-	    println!("Part 1: largest seat ID is {}", n);
-	}
-	None => {
-	    panic!("Part 1: there are no boarding passes!");
-	}
+fn decode_seat(directions: &str) -> Result<i32, &'static str> {
+    match (partition(0, 128, 'F', 'B', &directions[0..7]),
+	   partition(0, 8, 'L', 'R', &directions[7..])) {
+	(Ok(r), Ok(c)) => return Ok(seat_id(r, c)),
+	(Err(e), _) => Err(e),
+	(_, Err(e)) => Err(e),
     }
 }
 
-fn part2(seats: &BTreeSet<i32>) {
+fn part1(seats: &BTreeSet<i32>) -> Result<(), &'static str> {
+    match seats.iter().next_back() {
+	Some(n) => {
+	    println!("Part 1: largest seat ID is {}", n);
+	    Ok(())
+	}
+	None => Err("Part 1: there are no boarding passes!"),
+    }
+}
+
+fn part2(seats: &BTreeSet<i32>) -> Result<(), &'static str> {
     for seat in seats.iter() {
 	let following = seat + 1;
 	if !seats.contains(&following) {
 	    println!("Part 2: my seat is {}", following);
-	    return;
+	    return Ok(());
 	}
     }
-    panic!("Part 2: there are no gaps in the boarding passes");
+    Err("Part 2: there are no gaps in the boarding passes")
 }
 
 fn run() -> Result<(), String> {
-    let seats: Result<BTreeSet<i32>, _> = io::BufReader::new(io::stdin()).lines()
+    let seats_or_error: Result<BTreeSet<i32>, String> = io::BufReader::new(io::stdin()).lines()
 	.map(|x| match x {
 	    Err(e) => Err(format!("I/O error: {}", e)),
-	    Ok(line) => Ok(decode_seat(&line.as_str())),
+	    Ok(line) => decode_seat(&line.as_str()).map_err(str::to_string),
 	})
 	.collect();
-    match seats {
-	Ok(s) => {
-	    part1(&s);
-	    part2(&s);
-	    Ok(())
+    match seats_or_error {
+	Ok(seats) => {
+	    part1(&seats).map_err(str::to_string)?;
+	    part2(&seats).map_err(str::to_string)?;
+	    return Ok(());
 	}
 	Err(e) => Err(e),
     }
