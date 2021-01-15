6a18851f01236ff520d2d851bc67a28f63872bbe
James Youngman <james@youngman.org>
2021-01-15 10:50:01 +0000

    Day 24: solved part 2.

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -1,17 +1,14 @@
 extern crate log;
 extern crate pretty_env_logger;
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io::prelude::*;
 use std::io;
 
 #[derive(Debug,PartialEq,Eq)]
-enum Direction {		// note that N and S are deliberately missing.
-    NE,
-    E,
-    SE,
-    SW,
-    W,
-    NW,
+enum Direction {
+    // note that N and S are deliberately missing.
+    NE, E, SE, SW, W, NW,
 }
 
 impl Direction {
@@ -28,7 +25,7 @@ impl Direction {
     // You can visualise this as if alternate rows of hexagons have X
     // coordinates in between the X coordinates of the rows above and
     // below.
-    fn delta(&self) -> (i32,i32) { // (dx, dy)
+    fn delta(&self) -> (i32, i32) { // (dx, dy)
 	match self {
 	    Direction::NE => (1, 1),
 	    Direction::E => (2, 0),
@@ -38,9 +35,30 @@ impl Direction {
 	    Direction::NW => (-1, 1),
 	}
     }
+
+    fn all() -> &'static[Direction; 6] {
+	&[Direction::NE, Direction::E, Direction::SE,
+	  Direction::SW, Direction::W, Direction::NW]
+    }
 }
 
-#[derive(Debug,PartialEq,Eq,Hash)]
+fn split_directions(s: &str) -> Vec<Direction> {
+    // The capacity here is an overestimate, but by at most a factor
+    // of 2.
+    let mut result = Vec::with_capacity(s.len());
+    let mut saved: Option<char> = None;
+    for ch in s.chars() {
+	let (s, d) = get_direction(&saved, ch);
+	match d {
+	    None => (),
+	    Some(dir) => result.push(dir),
+	}
+	saved = s;
+    }
+    result
+}
+
+#[derive(Debug,Clone,Copy,PartialEq,Eq,Hash)]
 struct Position {
     x: i32,
     y: i32,
@@ -55,7 +73,6 @@ impl Position {
 	}
     }
 }
-
 
 struct Floor {
     // The default state of a tile is white, so we only record the
@@ -87,6 +104,35 @@ impl Floor {
 	    .fold(Position{x: 0, y: 0}, |p, d| p.delta(d));
 	self.flip(final_pos)
     }
+
+    fn iterate(&self) -> Floor {
+	let mut count_of_black_neighbours: HashMap<Position, usize> = HashMap::new();
+	for current_black_tile_pos in &self.black_tiles {
+	    for direction in Direction::all() {
+		let neighbour: Position = current_black_tile_pos.delta(direction);
+		(*count_of_black_neighbours.entry(neighbour).or_insert(0)) += 1;
+	    }
+	}
+	Floor{
+	    black_tiles: count_of_black_neighbours.iter()
+		.filter_map(|(pos, count)| {
+		    let is_currently_black = self.black_tiles.contains(pos);
+		    match count {
+			0|3|4|5|6 if is_currently_black => None, // turns white
+			2 if !is_currently_black => Some(pos),	 // turns black
+			_ => {
+			    if is_currently_black {
+				Some(pos) // stays black
+			    } else {
+				None // turns white
+			    }
+			}
+		    }
+		})
+		.copied()
+		.collect(),
+	}
+    }
 }
 
 fn get_direction(saved: &Option<char>, current: char) -> (Option<char>, Option<Direction>) {
@@ -108,39 +154,7 @@ fn get_direction(saved: &Option<char>, current: char) -> (Option<char>, Option<D
     }
 }
 
-fn split_directions(s: &str) -> Vec<Direction> {
-    let mut result = Vec::new();
-    let mut saved: Option<char> = None;
-    for ch in s.chars() {
-	let (s, d) = get_direction(&saved, ch);
-	match d {
-	    None => (),
-	    Some(dir) => result.push(dir),
-	}
-	saved = s;
-    }
-    result
-}
-
-
-fn solve1(lines: &Vec<String>) -> Floor {
-    let mut floor: Floor = Floor::new();
-    for line in lines {
-	floor.obey(line);
-    }
-    floor
-}
-
-
-
-
-fn part1(lines: &Vec<String>) -> Result<(), String> {
-    println!("Part 1: number of black-side-up tiles: {}",
-	     solve1(lines).count_black_tiles());
-    Ok(())
-}
-
-fn run() -> Result<(), String> {
+fn read_lines() -> Result<Vec<String>, String> {
     let mut lines: Vec<String> = Vec::new();
     for line_or_err in io::BufReader::new(io::stdin()).lines() {
 	match line_or_err {
@@ -152,8 +166,23 @@ fn run() -> Result<(), String> {
 	    }
 	}
     }
-    part1(&lines)?;
-    //part2(&lines)?;
+    Ok(lines)
+}
+
+fn run() -> Result<(), String> {
+    let lines = read_lines()?;
+    let mut floor: Floor = Floor::new();
+    for line in lines.iter() {
+	floor.obey(line);
+    }
+    println!("Part 1: number of black-side-up tiles: {}",
+	     floor.count_black_tiles());
+
+    for _ in 0..100 {
+	floor = floor.iterate();
+    }
+    println!("Part 2: number of black-side-up tiles: {}",
+	     floor.count_black_tiles());
     Ok(())
 }
 
