4d8aebcdd5abdbe9b18a92662cf73f4218e6e6d4
James Youngman <james@youngman.org>
2021-01-16 12:40:48 +0000

    Day 25: reduce causes of error by using an unsigned key type.

--- src/bin/day25.rs
+++ src/bin/day25.rs
@@ -4,13 +4,11 @@ extern crate pretty_env_logger;
 extern crate thiserror;
 
 use mod_exp::mod_exp;
-use std::convert::TryInto;
 use std::io::Read;
 use std::io;
 use thiserror::Error;
 
-type Key = i64;
-
+type Key = usize;
 
 #[derive(Error,Debug)]
 pub enum MyError {
@@ -19,12 +17,6 @@ pub enum MyError {
 
     #[error("Read error")]
     ReadError { source: std::io::Error },
-
-    #[error("Key {0} should not be negative: {1}")]
-    NegativeKey(i64, std::num::TryFromIntError),
-
-    #[error("numeric overflow")]
-    Overflow,
 }
 
 fn read_input() -> Result<Vec<Key>, MyError> {
@@ -46,9 +38,9 @@ fn read_input() -> Result<Vec<Key>, MyError> {
 // Finds r such that 7^r = p mod 20201227
 fn find_loop_num(p: Key) -> usize {
     let mut v = 1;
-    for loop_number in 0.. {
+    for r in 0.. {  // r is the loop number
 	if v == p {
-	    return loop_number;
+	    return r;
 	}
 	v = (v * 7) % 20201227;
     }
@@ -56,12 +48,8 @@ fn find_loop_num(p: Key) -> usize {
 }
 
 // Finds p = k^r mod 20201227
-fn make_private_key(k: Key, r: usize) -> Result<i64, MyError> {
-    match k.try_into() {
-	Ok(ku) => mod_exp::<usize>(ku, r, 20201227)
-	    .try_into().map_err(|_| MyError::Overflow),
-	Err(e) => { return Err(MyError::NegativeKey(k, e)) }
-    }
+fn make_private_key(k: Key, r: usize) -> Key {
+    mod_exp(k, r, 20201227)
 }
 
 
@@ -74,9 +62,9 @@ fn run() -> Result<(), MyError> {
 	println!("loop number is {:>8}", loop_num);
 	println!("");
     }
-    let ek0 = make_private_key(keys_and_loop_numbers[0].0, keys_and_loop_numbers[1].1)?;
+    let ek0 = make_private_key(keys_and_loop_numbers[0].0, keys_and_loop_numbers[1].1);
     println!("encryption key is {:>8}", ek0);
-    let ek1 = make_private_key(keys_and_loop_numbers[1].0, keys_and_loop_numbers[0].1)?;
+    let ek1 = make_private_key(keys_and_loop_numbers[1].0, keys_and_loop_numbers[0].1);
     assert_eq!(ek0, ek1);
     Ok(())
 }
